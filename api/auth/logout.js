8f4d82c0d448f4fd14182e9ed6c002bc9ff766f3
thienha336501903-a11y <thienha336501903@gmail.com>
2026-09-03 23:02:20 +0700

    security: harden System B Telegram response baseline (#54)

--- api/auth/logout.js
+++ api/auth/logout.js
@@ -1,6 +1,7 @@
 import { clearSessionCookie } from '../../lib/auth.js';
-import { json } from '../../lib/http.js';
+import { json, method } from '../../lib/http.js';
 export default async function handler(req, res) {
+  if (!method(req, res, ['POST'])) return;
   res.setHeader('Set-Cookie', clearSessionCookie());
   json(res, 200, { ok: true });
 }
